4db2f4de6d035fa905f5caee9f961af3f58b2183
kkozlovsky <kkozlovsky@yandex.ru>
2020-03-29 16:45:53 +0300

    Created admin components

---
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-admin-loyout',
+	templateUrl: './admin-layout.component.html',
+	styleUrls: ['./admin-layout.component.scss']
+})
+export class AdminLayoutComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
