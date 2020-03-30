5a002ea91debc4709c89421f6de3a83a33e0723a
kkozlovsky <kkozlovsky@yandex.ru>
2020-03-31 00:07:42 +0300

    Admin login page

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
 	selector: 'app-admin-loyout',
@@ -7,10 +8,14 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-	constructor() {
+	constructor(private router: Router) {
 	}
 
 	ngOnInit(): void {
 	}
 
+	logout(event: Event) {
+		event.preventDefault(); // отменяет поведение ссылки по умолчанию
+		this.router.navigate(['/admin', 'login']);
+	}
 }
