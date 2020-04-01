b15dbdb8591aa9de0637080d647890e64762b6df
kkozlovsky <kkozlovsky@yandex.ru>
2020-04-01 16:49:07 +0300

    Post list view

--- src/app/shared/components/post/post.component.ts
+++ src/app/shared/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../../interfaces';
 
 @Component({
 	selector: 'app-post',
@@ -9,6 +10,8 @@ export class PostComponent implements OnInit {
 
 	constructor() {
 	}
+
+	@Input() post: Post;
 
 	ngOnInit(): void {
 	}
